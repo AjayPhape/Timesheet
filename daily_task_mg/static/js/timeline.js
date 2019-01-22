25686c931683e876cce82c9aeb2588eb14b58891
ajay.phape <ajay.phape@matchpointgps.com>
2019-01-22 12:31:11 +0530

    [CHG]change jsgrid to datatable

---
+++ daily_task_mg/static/js/timeline.js
@@ -0,0 +1,83 @@
+      $(document).ready(function() {
+
+
+        $('#altEditor-modal [id="Work Date"]').attr({'type':'date'});
+        var data_list= [];
+        var columnDefs = [{
+        title:"rec_ID", visible: false,
+        searchable:false}, {
+          title: "Name"
+        }, {
+          title: "Consumed Hours"
+        }, {
+          title: "Work Date",
+        }
+        ];
+
+        var myTable = $('#example').DataTable({
+          "sPaginationType": "full_numbers",
+          "ajax": {
+                    "url":"http://10.200.234.215:9000/timesheet/api/?task_id=1",
+                    "type": "GET",
+                    "params": {"task_id":1}
+          },
+          columns: columnDefs,
+          dom: 'Bfrtip',        // Needs button container
+          select: 'single',
+          responsive: true,
+          altEditor: true,     // Enable altEditor
+          "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+                $(nRow).attr('id', aData[aData.length-1]);
+            },
+          buttons: [{
+            text: 'Add',
+            name: 'add'        // do not change name
+          },
+          {
+            extend: 'selected', // Bind to Selected row
+            text: 'Edit',
+            name: 'edit'        // do not change name
+          },
+          {
+            extend: 'selected', // Bind to Selected row
+            text: 'Delete',
+            name: 'delete'      // do not change name
+         }]
+
+        });
+        $('#altEditor-modal').on('click', '#addRowBtn', function(e){
+            $.ajax({
+                'url': "http://10.200.234.215:9000/timesheet/api/",
+                'method': "POST",
+                'data': {"consumed_hours":$('[id="Consumed Hours"]').val(),
+                        "work_date":$('[id="Work Date"]').val(),
+                        "name":$('#Name').val()}
+            });
+        });
+
+        $('#altEditor-modal').on('click', '#editRowBtn', function(e){
+           console.log($('#Name').val());
+            console.log($('[id="Consumed Hours"]').val());
+            console.log($('[id="Work Date"]').val());
+            console.log($('[id="id"]').val());
+
+            $.ajax({
+                'url': "http://10.200.234.215:9000/timesheet/api/",
+                'method': "PUT",
+                'data': {"rec_id":$('[id="rec_ID"]').val(),
+                        "consumed_hours":$('[id="Consumed Hours"]').val(),
+                        "work_date":$('[id="Work Date"]').val(),
+                        "name":$('#Name').val()}
+            });
+        });
+
+        $('#altEditor-modal').on('click', '#deleteRowBtn', function(e){
+            $.ajax({
+                'url': "http://10.200.234.215:9000/timesheet/api/",
+                'method': "DELETE",
+                'data': {"rec_id":$('[id="rec_ID"]').val()}
+            });
+        });
+
+
+      });
--- daily_task_mg/task_line_view.py
+++ daily_task_mg/task_line_view.py
@@ -1,4 +1,4 @@
-from django.http import request, HttpResponse
+from django.http import request, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.utils import timezone
 from django.core import serializers
@@ -8,35 +8,38 @@ from rest_framework.permissions import AllowAny
 from .models import TaskLine
 
 from datetime import datetime
-
+import json
 
 class TaskLineView(APIView):
 	permission_classes = (AllowAny, )
 
 	def get(self, request):
-		task_id = request.GET.get('task_id')
-		lines = TaskLine.objects.filter(task_id=task_id).order_by('work_date')
+		task_id = request.query_params.get('task_id')
+		data = TaskLine.objects.all().order_by('work_date').values_list('id', 'name', 'consumed_hours', 'consumed_hours')
+		# import pdb;pdb.set_trace()
 		print(request.GET, task_id)
-		# if request.query_params:
+		# if request.query_params:c
 		# 	lines = TaskLine.objects.all() \
 		# 		.filter(name__contains=request.GET.get('name')) \
 		# 		.filter(consumed_hours=request.GET.get('consumed_hours')) \
 		# 		.order_by('work_date')
-		data = serializers.serialize('json', lines)
+		# data = serializers.serialize('json', data)
+		# print(data)
+		data = json.dumps({"data":list(data)})
 		return HttpResponse(data, status=201)
 
 	def post(self, request):
-		print(datetime.strptime(request.data.get('work_date').replace(".000Z", ""), "%Y-%m-%dT%H:%M:%S"))
+		# print(datetime.strptime(request.data.get('work_date').replace(".000Z", ""), "%Y-%m-%dT%H:%M:%S"))
 		TaskLine.objects.create(
 			name=request.data.get('name'),
 			consumed_hours=request.data.get('consumed_hours'),
 			write_date=timezone.now(),
-			work_date=datetime.strptime(request.data.get('work_date').replace(".000Z", ""), "%Y-%m-%dT%H:%M:%S"),
+			# work_date=datetime.strptime(request.data.get('work_date').replace(".000Z", ""), "%Y-%m-%dT%H:%M:%S"),
 		).save()
 		return HttpResponse(status=201)
 
 	def put(self, request):
-		rec_id = request.GET.get('rec_id')
+		rec_id = request.data.get('rec_id')
 		print(rec_id)
 		line_obj = TaskLine.objects.filter(pk=rec_id)
 		print(request.data.get('work_date'))
@@ -44,12 +47,13 @@ class TaskLineView(APIView):
 			name=request.data.get('name'),
 			consumed_hours=request.data.get('consumed_hours'),
 			write_date=timezone.now(),
-			work_date=datetime.strptime(request.data.get('work_date').replace(".000Z", ""), "%Y-%m-%dT%H:%M:%S")
+			# work_date=datetime.strptime(request.data.get('work_date').replace(".000Z", ""), "%Y-%m-%dT%H:%M:%S")
 			)
 		return HttpResponse(status=200)
 
 	def delete(self, request):
-		rec_id = request.GET.get('rec_id')
+		rec_id = request.data.get('rec_id')
+		print(rec_id)
 		line_obj = TaskLine.objects.get(pk=rec_id)
 		line_obj.delete()
 		return HttpResponse(status=200)
@@ -57,4 +61,4 @@ class TaskLineView(APIView):
 
 def view_timeline(request, project_id=None, task_id=None):
 
-	return render(request, 'basic.html')
+	return render(request, 'index.html')
